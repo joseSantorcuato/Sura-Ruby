bc417e21b93a387baa43911784c97413eb440870
joseSantorcuato <santorcuato76@gmail.com>
2017-06-10 00:14:52 -0300

    Se agrega leer código fuente html

--- sesion2/bonus/leer_html.rb
+++ sesion2/bonus/leer_html.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'open-uri'
 
 
-titulo1 = "Curso Ruby 22\nJosé Santorcuato Tapia"
+titulo1 = "Curso Ruby Leer HTML\nJosé Santorcuato Tapia"
 titulo2 = "2017"
 titulo3 = "CHILE"
 
@@ -12,5 +12,5 @@ puts titulo3
 
 
 html = open('http://lun.cl').read
-puts html.length #=> 8461
-puts html        #=
+puts html.length
+puts html
