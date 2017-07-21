7f1cb194acd18a3a1fe19c9998caa255e63f36df
joseSantorcuato <santorcuato76@gmail.com>
2017-07-21 00:10:26 -0300

    Sesión 6.Ruby archivos docx y xlxs.Escritura.Lectura.Tablas.Formato

---
+++ sesion6/046_docx_html.rb
@@ -0,0 +1,15 @@
+
+titulo1 = "Curso Ruby 046\nJosé Santorcuato Tapia"
+titulo2 = "2017"
+titulo3 = "CHILE"
+
+puts titulo1
+puts titulo2
+puts titulo3
+
+require 'docx'
+
+doc = Docx::Document.open('prueba.docx')
+doc.paragraphs.each do |lectura|
+  puts lectura.to_html
+end
