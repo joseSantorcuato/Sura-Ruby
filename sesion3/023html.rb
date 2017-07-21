f13a1f125d179c3b5495f28bb64880fc7b3af4ea
joseSantorcuato <santorcuato76@gmail.com>
2017-07-21 00:03:24 -0300

    Sesion3.Trabajo sobre multimedia y gema GOSU.Imagen, sonido, fuentes.Lectura XML, JSON.

---
+++ sesion3/023html.rb
@@ -0,0 +1,18 @@
+require 'rubygems'
+require 'open-uri'
+
+
+titulo1 = "Curso Ruby 22\nJosé Santorcuato Tapia"
+titulo2 = "2017"
+titulo3 = "CHILE"
+
+puts titulo1
+puts titulo2
+puts titulo3
+
+
+html = open('http://lun.cl').read
+puts html.length #=> 8461
+puts html.class #=> 8461
+puts html.size #=> 8461
+puts html        #=
