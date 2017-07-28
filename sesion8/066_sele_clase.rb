32d2c6d62d0b8b231888f2ae8cf9a0867a596993
joseSantorcuato <santorcuato76@gmail.com>
2017-07-28 03:28:05 -0400

    Se agrega sesión 8.QA.Automatización de pruebas.Instalación webdrivers IE, Ch, MO. Gema Selenium y Capybara.Pruebas sitios

---
+++ sesion8/066_sele_clase.rb
@@ -0,0 +1,70 @@
+
+titulo1 = "Curso Ruby 66\nJosé Santorcuato Tapia"
+titulo2 = "2017"
+titulo3 = "CHILE"
+
+puts titulo1
+puts titulo2
+puts titulo3
+
+espacio = " \n"
+puts espacio
+
+  require_relative "./claseSeledroid"
+
+robot =  Seledroid.new()
+robot.ejecutar
+
+  session = Capybara::Session.new(:selenium)
+  session.visit "http://calculadora-uf.cl/"
+
+
+  # id 'id' #clase fijarse abajo, cuando es clase lleva # cuando es id no
+
+
+session.fill_in('input_UF', :with => 3)
+sleep 1
+
+
+valor =  session.find("#result_CLP.result").text #al concatenar id y clase utilizar # y .
+conversiona = valor.sub!(/,/, "")
+conversionb = '%.3f' % (conversiona)
+a_decimal= conversionb.to_f
+
+decimal_sin_coma = (a_decimal * 1000).floor
+
+
+
+
+
+puts session.find("#result_CLP.result").text #al concatenar id y clase utilizar # y .
+
+sleep 1
+
+session.fill_in('input_CLP', :with => decimal_sin_coma)
+sleep 1
+
+session.find("#result_UF.result").text #al concatenar id y clase utilizar # y .puts session.find("#result_CLP.result").text #al concatenar id y clase utilizar # y .
+
+
+sleep 1
+
+
+
+
+
+
+
+  #btn btn-primary entry-account
+
+
+
+  Capybara::Selenium::Driver.class_eval do
+    def quit
+      $stdin.gets
+      @browser.quit
+
+    rescue Errno::ECONNREFUSED
+      # Browser must have already gone
+    end
+  end
