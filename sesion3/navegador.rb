2708fc60acbd42f9d9e07788e170219aae340f20
joseSantorcuato <santorcuato76@gmail.com>
2017-07-27 22:49:09 -0300

    Mantención

--- sesion3/navegador.rb
+++
@@ -1,8 +0,0 @@
-bienvenida = "NAVEGADOR - \nTutorial de Ruby \nJosé Santorcuato Tapia\nChile 2017"
-puts bienvenida
-
-espacio = " \n"
-  puts espacio
-  
-
-  system("chromium-browser http://www.arselectronicachile.blogspot.com")# cambia chromium-browser por firefox
